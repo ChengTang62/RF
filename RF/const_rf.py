84268ce12a8c5e4ea8cd662f1e32f351778a5789
Cheng Tang <71523150+ChengTang62@users.noreply.github.com>
2024-10-17 19:34:35 -0400

    Update const_rf.py

--- RF/const_rf.py
+++ RF/const_rf.py
@@ -3,17 +3,17 @@ BASE_DIR = abspath(join(dirname(__file__), pardir))
 output_dir = join(BASE_DIR, 'RF/dataset/')
 split_mark = '\t'
 OPEN_WORLD = False
-MONITORED_SITE_NUM = 95
-MONITORED_INST_NUM = 1000
+MONITORED_SITE_NUM = 4
+MONITORED_INST_NUM = 100
 UNMONITORED_SITE_NUM = 40000
 UNMONITORED_SITE_TRAINING = 900
 model_path = 'pretrained/'
 
-num_classes = 95
-num_classes_ow = 96
+num_classes = 4
+num_classes_ow = 0
 # Length of TAM
-max_matrix_len = 1800
+max_matrix_len = 18000
 # Maximum Load Time
-maximum_load_time = 80
+maximum_load_time = 800
 
-max_trace_length = 5000
+max_trace_length = 100000
